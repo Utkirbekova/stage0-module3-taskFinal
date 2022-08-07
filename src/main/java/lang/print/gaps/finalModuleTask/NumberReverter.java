b6623c1bfa8ccbf857e0419472859c43c0d4a78d
Madinabonu <otkirbekovamadinabonu@gmail.com>
2022-08-07 19:56:56 +0300

    .

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+
+        int rev_num = 0;
+        while (number > 0) {
+            rev_num = rev_num * 10 + number % 10;
+            number = number / 10;
+        }
+        System.out.println(rev_num);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -1,6 +1,10 @@
 package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
-    public void toFahrenheit(int temperatureCelsius){
+    public void toFahrenheit(float temperatureCelsius) {
+
+        //(0°C × 9/5) + 32
+        float temperatureF = temperatureCelsius * 9 / 5 + 32;
+        System.out.println(temperatureF);
     }
 }
